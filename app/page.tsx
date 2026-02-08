8618787a364f9ab03835afa55146cdfd6d77a3f8
devdavidias <davitdt206@email.com>
2026-02-08 17:17:11 -0300

    build: estrutura de pastas corrigida e app na raiz

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Template from "@/src/app/Template/pages"; // Verifique se o caminho do import está correto
+import Template from "@/app/Template/pages"; // Verifique se o caminho do import está correto
 
 export default function Home() {
   return (
     <Template>
-      {/* O que você colocar aqui aparecerá quando a 'sessao' for 'home' */}
       <div className="flex flex-col items-center justify-center min-h-[60vh] text-center">
         <h2 className="text-4xl font-bold text-white mb-4">
           Bem-vindo ao <span className="text-blue-500">emti.dev</span>
